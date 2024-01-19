dc71b8a8d236de7e104f5cc5147df9e1500feeb0
VanY <apple@APPLEs-MacBook-Pro.local>
2024-01-19 14:16:01 +0200

    [feat]: initialized project

--- src/main/java/com/jazzybruno/example/SpringApplication.java
+++ src/main/java/com/jazzybruno/example/SpringApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringApplication {
-
     public static void main(String[] args) {
         org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
     }
