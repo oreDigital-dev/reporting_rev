cd5c2ec4b61cdc75c65915ff1efd97668c06e6e2
VanY <apple@APPLEs-MacBook-Pro.local>
2024-01-30 09:28:55 +0200

    [fix]: docker file

--- src/main/java/rw/oredigital/com/v1/repositories/ILocationRepository.java
+++ src/main/java/rw/oredigital/com/v1/repositories/ILocationRepository.java
@@ -4,12 +4,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rw.oredigital.com.v1.models.location.Location;
 import rw.oredigital.com.v1.models.location.LocationLevel;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface ILocationRepository extends JpaRepository<Location, Integer> {
     Optional<Location> findByCode(String code);
+
     Page<Location> findAll(Pageable pageable);
     boolean existsByCode(String code);
     List<Location> findAllByParent(Location location);
--- src/main/java/rw/oredigital/com/v1/repositories/UserRepository.java
+++ src/main/java/rw/oredigital/com/v1/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package rw.oredigital.com.v1.repositories;
 
+import org.springframework.stereotype.Repository;
 import rw.oredigital.com.v1.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
 }
